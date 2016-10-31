5477bbc9ad6b0bfcd90ef853ab32ab954af1dab4
Ken Hwong <ken_hwong@msn.com>
2016-10-31 17:29:07 +0800

    1031 2

--- XDATA/App.xaml.cs
+++ XDATA/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using XDATA.Services;
 
 namespace XDATA
 {
@@ -13,5 +14,12 @@ namespace XDATA
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Ioc.RegisterInheritedTypes(typeof(ServiceBase).Assembly, typeof(ServiceBase));
+            SettingContext.Instance.Init();
+
+            base.OnStartup(e);
+        }
     }
 }
