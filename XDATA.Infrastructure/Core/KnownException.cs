bc5f8134b267366d8c6df803e3653d43158668fe
Ken Hwong <ken_hwong@msn.com>
2016-11-01 17:02:32 +0800

    1101

--- XDATA.Infrastructure/Core/KnownException.cs
+++ XDATA.Infrastructure/Core/KnownException.cs
@@ -2,6 +2,7 @@
 
 namespace XDATA
 {
+    [Serializable]
     public class KnownException : Exception
     {
         public KnownException(string message) : base(string.IsNullOrEmpty(message) ? "Unknown Exception" : message)
